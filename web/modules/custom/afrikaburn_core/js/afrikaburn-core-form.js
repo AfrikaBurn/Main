2fc5323f289b0dc96c44bbbfba96d19db8c0d2d2
Dev Server <alastair@afrikaburn.com>
2017-08-18 10:25:26 +0000

    Added validation behaviours to wizard wforms

--- web/modules/custom/afrikaburn_core/js/afrikaburn-core-form.js
+++ web/modules/custom/afrikaburn_core/js/afrikaburn-core-form.js
@@ -3,7 +3,7 @@
  * AfrikaBurn core behaviors.
  */
 
-(function ($) {
+(function ($, toValidate) {
 
   'use strict';
 
@@ -11,7 +11,7 @@
     attach: function (context, settings) {
 
       // Form validation
-      $('.form-text, .form-autocomplete, .form-checkbox, .form-select, .form-textarea, .form-file', context).blur(
+      $(toValidate, context).blur(
         function(){
           $(this).valid();
         }
@@ -27,36 +27,77 @@
     }
   };
 
+  // Adds basic Next/Previous button behaviour to tabbed forms with the js-wizard class
   class Wizard {
 
     constructor(element){
 
       this.root = $(element);
-      this.tabs = $('.vertical-tabs__menu', this.root).children();
-      this.panels = $('.field-group-tab .details-wrapper', this.root);
+      this.tabs = $('.vertical-tabs__menu, .horizontal-tabs-list', this.root).children();
+      this.panels = $('.field-group-tab > .details-wrapper', this.root);
 
       this.panels.append('<div class="wizard-actions"></div>');
-      this.panels.not(':first').find('.wizard-actions').append('<input type="button" value="Previous" class="js-previous button" />');
-      this.panels.not(':last').find('.wizard-actions').append('<input type="button" value="Next" class="js-next button" />');
       this.root.parents('form').find('.form-actions').appendTo(this.panels.last());
+      this.attachPrevious();
+      this.attachNext();
 
-      $('.js-next', this.root).click(
-        () => {
-          $(this.tabs.filter('.is-selected')).next().find('a').click();
-          $('html, body').animate({ scrollTop: this.root.offset().top - 200 }, 500);
-        }
-      );
+    }
 
+    // Attach the previous buttons and behaviours
+    attachPrevious(){
+      this.panels.not(':first').find('.wizard-actions').append('<input type="button" value="Previous" class="js-previous button" />');
       $('.js-previous', this.root).click(
         () => {
-          $(this.tabs.filter('.is-selected')).prev().find('a').click();
-          $('html, body').animate({ scrollTop: this.root.offset().top - 200 }, 500);
+          this.getActiveTab().prev().find('a').click();
+          this.scrollTo(this.root);
         }
       );
+    }
 
+    // Attach the next buttons and behaviours
+    attachNext(){
+      this.panels.not(':last').find('.wizard-actions').append('<input type="button" value="Next" class="js-next button" />');
+      $('.js-next', this.root).click(
+        () => {
+          if (this.validate()) {
+            this.getActiveTab().next().find('a').click();
+            this.scrollTo(this.root);
+          } else {
+            this.scrollTo(this.getVisibleErrors()[0]);
+          }
+        }
+      );
+    }
+
+    // Validate the current tab
+    validate(){
+      var activePanel = this.getActivePanel();
+      activePanel.find(toValidate).valid();
+      return this.getVisibleErrors().length < 1;
+    }
+
+    // Scroll to top of element
+    scrollTo(element){
+      var top = $(element).offset().top - parseInt($('body').css('margin-top')) - parseInt($('body').css('padding-top'));
+      $('html, body').animate({ scrollTop: top - 30}, 500);
+    }
+
+    // Get the active tab
+    getActiveTab(){
+      return this.tabs.filter('.is-selected, .selected');
+    }
+
+    // Get the active panel
+    getActivePanel(){
+      return $(this.panels[this.getActiveTab().index()]);
+    }
+
+    // Get all visible errors on the active panel
+    getVisibleErrors(){
+      return this.getActivePanel().find('.error:visible');
     }
 
   }
 
-})(jQuery);
+})(jQuery, '.form-text, .form-autocomplete, .form-checkbox, .form-select, .form-textarea, .form-file, .form-number, .form-date');
 
