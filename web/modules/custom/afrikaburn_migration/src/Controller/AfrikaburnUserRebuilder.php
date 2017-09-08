0e5f154e51b7886087a8dcb979f09668955d03a5
Scheepers <scheepers@native.co.za>
2017-09-08 13:49:00 +0200

    jissisfoktog

--- web/modules/custom/afrikaburn_migration/src/Controller/AfrikaburnUserRebuilder.php
+++ web/modules/custom/afrikaburn_migration/src/Controller/AfrikaburnUserRebuilder.php
@@ -15,29 +15,11 @@ class AfrikaburnUserRebuilder extends ControllerBase {
   public static function rebuildUser($uid, $agreements, &$context) {
 
     $user = \Drupal::entityTypeManager()->getStorage('user')->load($uid);
-    // $user->langcode = 'en';
-    // $user->preferred_langcode = 'en';
-    // $user->admin_langcode = NULL;
-    $user->field_agreements = array_keys($agreements);
+    $user->langcode = 'en';
+    $user->preferred_langcode = 'en';
+    $user->admin_langcode = NULL;
 
-    // $insert = \Drupal::database()
-    //   ->insert('webform_submission')
-    //   ->fields(['uuid', 'webform_id', 'uid', 'entity_id']);
-    // foreach($agreements as $agreement){
-    //   foreach($agreement->field_agreement_terms->referencedEntities() as $webform){
-    //     $insert->values(
-    //       [
-    //         \Drupal::service('uuid')->generate(), 
-    //         $webform->id(), 
-    //         $user->id(), 
-    //         $agreement->id()
-    //       ]
-    //     );
-    //   }
-    // }
-    // $insert->execute();
-
-    $context['results'][] = $user->save();
+    $context['results'][] = $user->save();3
     $context['message'] = 'Rebuilding Users';
 
   }
@@ -50,7 +32,7 @@ class AfrikaburnUserRebuilder extends ControllerBase {
         'One User processed.', '@count users rebuilt.'
       );
     } else {
-      $message = t('Finished with an error.');
+      $message = t('Finished with errors.');
     }
 
     drupal_set_message($message);
