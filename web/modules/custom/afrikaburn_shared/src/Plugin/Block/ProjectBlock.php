2df341426375f2fd8627e6b04377b9ee6dd78cec
Scheepers <scheepers@native.co.za>
2017-09-15 11:05:00 +0200

    Added custom block

---
+++ web/modules/custom/afrikaburn_shared/src/Plugin/Block/ProjectBlock.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\afrikaburn_shared\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "project_block",
+ *   admin_label = @Translation("Project Block"),
+ *   category = @Translation("Project Block"),
+ * )
+ */
+class ProjectBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $block = [
+      '#type' => 'markup',
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if ($node) { 
+      $nid = $node->id();
+      $block['#markup'] = '
+        <ul>
+          <li><a href="/node/add/art?field_collective=' . $nid . '" class="button">An artwork</a></li>        
+          <li><a href="/node/add/performances?field_collective=' . $nid . '" class="button">A Performance</a></li>
+          <li><a href="/node/add/theme_camps?field_collective=' . $nid . '" class="button">A Theme camp</a></li>
+        </ul>
+      ';
+    }
+
+    return $block;
+  }
+}
