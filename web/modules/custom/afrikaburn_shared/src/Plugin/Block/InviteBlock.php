adcc6f534fbd81880f724d7885c97a45caea51cb
Scheepers <scheepers@native.co.za>
2017-09-21 23:36:42 +0200

    Fixed members view

--- web/modules/custom/afrikaburn_shared/src/Plugin/Block/InviteBlock.php
+++ web/modules/custom/afrikaburn_shared/src/Plugin/Block/InviteBlock.php
@@ -25,28 +25,32 @@ class InviteBlock extends BlockBase {
 
     $form = \Drupal::service('access_manager')->checkNamedRoute('afrikaburn_shared.invite', ['cid' => $collective->id()], $user) 
       ? [
-          '#type' => 'form',
-          '#action' => '/collective/' . $collective->id() . '/invite',
-          
-          'emails' => [
-            '#type' => 'textfield',
-            '#attributes' => [
-              'size' => 34,
-              'placeholder' => 'john@smith.com, ncedi@shaya.com...',
-              'name' => 'emails',
-            ],
+        '#type' => 'form',
+        '#action' => '/collective/' . $collective->id() . '/invite',
+        
+        'emails' => [
+          '#type' => 'textfield',
+          '#attributes' => [
+            'size' => 34,
+            'placeholder' => 'john@smith.com, ncedi@shaya.com...',
+            'name' => 'emails',
           ],
+        ],
 
-          'submit' => [
-            '#type' => 'submit',
-            '#value' => 'Send',
-          ],
+        'submit' => [
+          '#type' => 'submit',
+          '#value' => 'Send',
+        ],
 
-          '#cache' => [
-            'max-age' => 0,
-          ],
-        ] 
-      : [];
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ] 
+      : [
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ];
 
     return $form;
   }
