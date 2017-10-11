84087525731a92da8bc4b897a059e1620828d9bb
Ubuntu <ubuntu@ip-172-31-26-1.us-west-2.compute.internal>
2017-10-11 10:28:06 +0000

    Current live state

---
+++ web/user-export.sql
@@ -0,0 +1,23 @@
+SELECT
+  field_first_name_value,
+  field_last_name_value,
+  mail,
+  field_mobile_value,
+  field_id_number_value
+
+FROM
+  d8_newusers users,
+  d8_newuser__field_first_name first_name,
+  d8_newuser__field_last_name last_name,
+  d8_newusers_field_data mail,
+  d8_newuser__field_mobile mobile,
+  d8_newuser__field_id_number id
+
+WHERE
+  users.uid > '1' AND
+  users.uid = first_name.entity_id AND
+  users.uid = last_name.entity_id AND
+  users.uid = mail.uid AND
+  users.uid = mobile.entity_id AND
+  users.uid = id.entity_id
+
